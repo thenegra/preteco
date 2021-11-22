27b417ba27b11f4ab0742740d6a68f51ee3ee558
thenegra <ssanchez@thenegra.com>
2021-11-22 19:39:54 -0300

    asdf
    asdf

--- template-parts/block/content-mod-texto.php
+++ template-parts/block/content-mod-texto.php
@@ -1,8 +1,7 @@
 <!-- Bloque de texto -->
 </div>
 
-<section class="text-block bg-blanco">
-
+<section class="text-block bg-<?php if(get_field('color')){ echo get_field('color'); } else{ echo 'blanco';}?>">
 	<div class="container">
 		<article class="text-container">
 			<h3 class="tit-two"><?php echo get_field('titulo');?></h3>
