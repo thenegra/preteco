5cedd15bf4a8eeb7ad69e9a92798b58d157052b8
thenegra <ssanchez@thenegra.com>
2021-12-29 12:39:39 -0300

    Update content-mod-hero.php

--- template-parts/block/content-mod-hero.php
+++ template-parts/block/content-mod-hero.php
@@ -22,7 +22,8 @@
 							
 						</article>
 						<div class="img-container">
-						<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="Image placeholder">
+						<!--img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="Image placeholder"-->
+						<?php echo wp_get_attachment_image($contenido['foto']['ID'],'medium'); ?>
 						</div>
 					</li>
 					<?php endforeach; ?>
