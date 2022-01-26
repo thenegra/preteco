2130ee2ea71819628ae0f8cdcff260b350a88b28
thenegra <ssanchez@thenegra.com>
2022-01-26 14:58:51 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -346,53 +346,6 @@ function sg_get_first_block_id() {
     }
 }
 
-
-
-
-
-
-/**
- * Display a custom taxonomy dropdown in admin
- * @author Mike Hemberger
- * @link http://thestizmedia.com/custom-post-type-filter-admin-custom-taxonomy/
- */
-add_action('restrict_manage_posts', 'tsm_filter_post_type_by_taxonomy');
-function tsm_filter_post_type_by_taxonomy() {
-	global $typenow;
-	$post_type = 'team'; // change to your post type
-	$taxonomy  = 'group'; // change to your taxonomy
-	if ($typenow == $post_type) {
-		$selected      = isset($_GET[$taxonomy]) ? $_GET[$taxonomy] : '';
-		$info_taxonomy = get_taxonomy($taxonomy);
-		wp_dropdown_categories(array(
-			'show_option_all' => sprintf( __( 'Show all %s', 'textdomain' ), $info_taxonomy->label ),
-			'taxonomy'        => $taxonomy,
-			'name'            => $taxonomy,
-			'orderby'         => 'name',
-			'selected'        => $selected,
-			'show_count'      => true,
-			'hide_empty'      => true,
-		));
-	};
-}
-/**
- * Filter posts by taxonomy in admin
- * @author  Mike Hemberger
- * @link http://thestizmedia.com/custom-post-type-filter-admin-custom-taxonomy/
- */
-add_filter('parse_query', 'tsm_convert_id_to_term_in_query');
-function tsm_convert_id_to_term_in_query($query) {
-	global $pagenow;
-	$post_type = 'team'; // change to your post type
-	$taxonomy  = 'group'; // change to your taxonomy
-	$q_vars    = &$query->query_vars;
-	if ( $pagenow == 'edit.php' && isset($q_vars['post_type']) && $q_vars['post_type'] == $post_type && isset($q_vars[$taxonomy]) && is_numeric($q_vars[$taxonomy]) && $q_vars[$taxonomy] != 0 ) {
-		$term = get_term_by('id', $q_vars[$taxonomy], $taxonomy);
-		$q_vars[$taxonomy] = $term->slug;
-	}
-}
-
-
 add_filter('use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2);
 function prefix_disable_gutenberg($current_status, $post_type)
 {
@@ -402,4 +355,4 @@ function prefix_disable_gutenberg($current_status, $post_type)
 }
 
 
-?>
+
